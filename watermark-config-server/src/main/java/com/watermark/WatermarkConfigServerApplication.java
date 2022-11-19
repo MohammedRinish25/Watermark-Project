a4735b704aa80ca88204f4f43f45c72f0c036f43
MohammedRinishA <MohammedRinishA@Rinish>
2022-11-19 10:50:58 +0530

    first commit

---
+++ watermark-config-server/src/main/java/com/watermark/WatermarkConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.watermark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class WatermarkConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatermarkConfigServerApplication.class, args);
+	}
+
+}
