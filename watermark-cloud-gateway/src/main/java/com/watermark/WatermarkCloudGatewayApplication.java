0dd53da1fdd3cb0900bf00aac11a11492ca1cbe4
MohammedRinishA <MohammedRinishA@Rinish>
2022-11-19 10:53:58 +0530

    first commit

---
+++ watermark-cloud-gateway/src/main/java/com/watermark/WatermarkCloudGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.watermark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class WatermarkCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatermarkCloudGatewayApplication.class, args);
+	}
+
+}
